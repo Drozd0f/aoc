8b88ed7b95e47765876494a70e12442a39441f0d
Drozd0f <600dan600@gmail.com>
2023-12-05 11:20:07 +0200

    Year 2023 day 5

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"github.com/Drozd0f/year2023/day5"
+	"github.com/Drozd0f/year2023/reader"
+)
+
 func main() {
+	//fmt.Println(day5.CalculatePart1(reader.ReadAllFile("year2023/day5/input/input.txt")))
+	//fmt.Println(day5.CalculatePart1(reader.ReadAllFile("year2023/day5/input/example_part_1.txt")))
+	//fmt.Println(day5.CalculatePart2(reader.ReadAllFile("year2023/day5/input/example_part_1.txt")))
+	fmt.Println(day5.CalculatePart2(reader.ReadAllFile("year2023/day5/input/input.txt")))
 }
