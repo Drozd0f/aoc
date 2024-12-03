a8d4f36161258c914ac77cd2aa3ac03e1110c322
d.drozdov <d.drozdov@evopay.com.ua>
2024-12-03 13:19:43 +0200

    Year-2024: Day 3

---
+++ year2024/day3/day3_test.go
@@ -0,0 +1,37 @@
+package day3
+
+import (
+	"testing"
+)
+
+// BenchmarkSolutionForPartOne-12    	 1884061	       622.3 ns/op
+func BenchmarkSolutionForPartOne(b *testing.B) {
+	input := []string{
+		"7 6 4 2 1",
+		"1 2 7 8 9",
+		"9 7 6 2 1",
+		"1 3 2 4 5",
+		"8 6 4 4 1",
+		"1 3 6 7 9",
+	}
+
+	for range b.N {
+		SolutionForPartOne(input)
+	}
+}
+
+// BenchmarkSolutionForPartTwo-12    	 1437045	       825.0 ns/op
+func BenchmarkSolutionForPartTwo(b *testing.B) {
+	input := []string{
+		"7 6 4 2 1",
+		"1 2 7 8 9",
+		"9 7 6 2 1",
+		"1 3 2 4 5",
+		"8 6 4 4 1",
+		"1 3 6 7 9",
+	}
+
+	for range b.N {
+		SolutionForPartTwo(input)
+	}
+}
